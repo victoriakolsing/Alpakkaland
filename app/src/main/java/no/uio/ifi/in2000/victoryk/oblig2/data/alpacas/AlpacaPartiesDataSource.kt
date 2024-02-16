0bca1c1d2b05158c9404ebb72e9f05628ee61509
Victoria Kolsing <victoria.kolsing@hotmail.com>
2024-02-16 13:40:15 +0100

    Made API call in try catch in data source

--- app/src/main/java/no/uio/ifi/in2000/victoryk/oblig2/data/alpacas/AlpacaPartiesDataSource.kt
+++ app/src/main/java/no/uio/ifi/in2000/victoryk/oblig2/data/alpacas/AlpacaPartiesDataSource.kt
@@ -1,13 +1,28 @@
 package no.uio.ifi.in2000.victoryk.oblig2.data.alpacas
 
+import android.util.Log
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import no.uio.ifi.in2000.victoryk.oblig2.model.alpacas.PartyInfo
 
+val BASE_URL = "https://www.uio.no/studier/emner/matnat/ifi/IN2000/v24/obligatoriske-oppgaver/alpacaparties.json"
 @Serializable
-data class AlpacaPartiesDataSource (
-    val id: String,
-    val name: String,
-    val leader: String,
-    val img: String,
-    val color: String,
-    val description: String
-)
+class AlpacaPartiesDataSource {
+    suspend fun getPartyInfoList(): List<PartyInfo> {
+        return withContext(Dispatchers.Default) {
+            try {
+                val response = HttpClient().get(BASE_URL)
+                Json.decodeFromString<List<PartyInfo>>(response.toString())
+            } catch (ex: Exception) {
+                Log.e("OOPS", "sum ting wong with api call")
+                emptyList()
+            }
+        }
+    }
+
+}
+
