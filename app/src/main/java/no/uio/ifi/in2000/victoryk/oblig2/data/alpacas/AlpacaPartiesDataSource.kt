b076d2cbd08bede19421ab6121f75d558b3195b1
Victoria Kolsing <victoria.kolsing@hotmail.com>
2024-02-16 14:48:44 +0100

    Fixed alpaca card taking partyInfo object as argument

--- app/src/main/java/no/uio/ifi/in2000/victoryk/oblig2/data/alpacas/AlpacaPartiesDataSource.kt
+++ app/src/main/java/no/uio/ifi/in2000/victoryk/oblig2/data/alpacas/AlpacaPartiesDataSource.kt
@@ -9,13 +9,15 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import no.uio.ifi.in2000.victoryk.oblig2.model.alpacas.PartyInfo
 
-val BASE_URL = "https://www.uio.no/studier/emner/matnat/ifi/IN2000/v24/obligatoriske-oppgaver/alpacaparties.json"
 @Serializable
 class AlpacaPartiesDataSource {
+
+    private val url = "https://www.uio.no/studier/emner/matnat/ifi/IN2000/v24/obligatoriske-oppgaver/alpacaparties.json"
+
     suspend fun getPartyInfoList(): List<PartyInfo> {
         return withContext(Dispatchers.Default) {
             try {
-                val response = HttpClient().get(BASE_URL)
+                val response = HttpClient().get(url)
                 Json.decodeFromString<List<PartyInfo>>(response.toString())
             } catch (ex: Exception) {
                 Log.e("OOPS", "sum ting wong with api call")
--- app/src/main/java/no/uio/ifi/in2000/victoryk/oblig2/ui/home/HomeScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/victoryk/oblig2/ui/home/HomeScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -19,17 +20,21 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
+import kotlinx.coroutines.flow.StateFlow
+import no.uio.ifi.in2000.victoryk.oblig2.model.alpacas.PartyInfo
 
 // Viser kortene i en LazyColumn eller LazyVerticalGrid.
 // Denne skal kun observere UI-staten som ligger i HomeViewModel
 
 @Composable
-fun AlpacaCard() {
+fun AlpacaCard(partyInfo: PartyInfo, onClick: (PartyInfo) -> Unit) {
 
     val temporaryPartyName = "Party name"
     val temporaryImageURL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQAWoLZpHFCADozs4_TEvQwAXwEURTgakqV-9auJ_klHJcF1Wrxg&s"
     val temporaryLeaderName = "Leader"
     val temporaryPartyColor: Color = Color.Red
+
+    // REMEMBER TO ADD HANDLING FOR IMAGES AND COLOURS AND SUCH
 
     Card(
         modifier = Modifier.size(150.dp),
@@ -39,16 +44,16 @@ fun AlpacaCard() {
             verticalArrangement = Arrangement.SpaceEvenly,
             modifier = Modifier.fillMaxSize()
         ) {
-            Text(text = temporaryPartyName)
+            Text(text = partyInfo.name)
             Image(
-                painter = rememberAsyncImagePainter(temporaryImageURL),
+                painter = rememberAsyncImagePainter(partyInfo.img),
                 contentDescription = null,
                 modifier = Modifier
                     .size(64.dp)
                     .clip(CircleShape)
-                    .border(2.dp, color = temporaryPartyColor, CircleShape)
+                    .border(2.dp, color = Color(partyInfo.color.toInt()), CircleShape)
             )
-            Text(text = temporaryLeaderName)
+            Text(text = partyInfo.leader)
         }
     }
 }
@@ -57,17 +62,25 @@ fun AlpacaCard() {
 @Preview
 @Composable
 fun PreviewAlpacaCard() {
-    AlpacaCard()
 }
 
 @Composable
 fun HomeScreen(viewModel: HomeViewModel) {
-    val partyList = viewModel.partyList
 
+    val partyListState: StateFlow<List<PartyInfo>> = viewModel.partyList
+    val partyList = partyListState.collectAsState(initial = emptyList())
+
+
+}
+
+
+@Composable
+fun PartyList(parties: List<PartyInfo>, onPartyClicked: (PartyInfo) -> Unit) {
     LazyColumn {
-        items(partyList.value) { party ->
-            Text(text = party.toString())
-            // Render each party info item
+        items(parties) { party ->
+            AlpacaCard(partyInfo = party) {
+                onPartyClicked(party)
+            }
         }
     }
 }
