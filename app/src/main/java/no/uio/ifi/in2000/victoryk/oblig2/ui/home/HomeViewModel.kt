cee02efac50b76788d5a6c5dae38fbd2668092b9
Victoria Kolsing <victoria.kolsing@hotmail.com>
2024-02-16 14:32:58 +0100

    Fixing type mismatch in HomeViewModel

--- app/src/main/java/no/uio/ifi/in2000/victoryk/oblig2/ui/home/HomeViewModel.kt
+++ app/src/main/java/no/uio/ifi/in2000/victoryk/oblig2/ui/home/HomeViewModel.kt
@@ -1,21 +1,31 @@
 package no.uio.ifi.in2000.victoryk.oblig2.ui.home
 
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import no.uio.ifi.in2000.victoryk.oblig2.data.alpacas.AlpacaPartiesRepository
 import no.uio.ifi.in2000.victoryk.oblig2.model.alpacas.PartyInfo
 
+// gir type mismatch i getPartyList()
+/*
+data class partyList (
+    val parties: List<PartyInfo> = emptyList()
+)
+*/
+
+
 class HomeViewModel(private val repo: AlpacaPartiesRepository): ViewModel() {
 
-    // STUPID STATE HOLDERS
-    private val _partyList: MutableState<List<PartyInfo>> = mutableStateOf(emptyList())
-    val partyList: State<List<PartyInfo>> = _partyList
+    /*
+    private val _partyList = MutableStateFlow(partyList())
+    val partyList: StateFlow<partyList> = _partyList.asStateFlow()
+    */
 
-    // FETCH DATA FROM REPOSITORY
+    private val _partyList = MutableStateFlow<List<PartyInfo>>(emptyList())
+    val partyList: StateFlow<List<PartyInfo>> = _partyList
+
     private fun getPartyList() {
         viewModelScope.launch {
             _partyList.value = repo.getParties()
