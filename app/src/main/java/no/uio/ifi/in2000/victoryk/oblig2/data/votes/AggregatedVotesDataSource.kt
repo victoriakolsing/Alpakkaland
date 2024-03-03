14a4e66c7da7919180d42cb84718db0af8f9d4f3
Victoria Kolsing <victoria.kolsing@hotmail.com>
2024-03-03 18:32:17 +0100

    VoteList displays votes - also District 3

--- app/src/main/java/no/uio/ifi/in2000/victoryk/oblig2/data/votes/AggregatedVotesDataSource.kt
+++ app/src/main/java/no/uio/ifi/in2000/victoryk/oblig2/data/votes/AggregatedVotesDataSource.kt
@@ -8,13 +8,12 @@ import io.ktor.client.request.get
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
+import no.uio.ifi.in2000.victoryk.oblig2.model.votes.AggregatedVotes
 import no.uio.ifi.in2000.victoryk.oblig2.model.votes.District
 import no.uio.ifi.in2000.victoryk.oblig2.model.votes.DistrictVotes
 
 @Serializable
-data class PartiesD (
-    val parties: List<DistrictVotes>
-)
+data class PartiesVote(val parties: List<AggregatedVotes>)
 
 class AggregatedVotesDataSource {
     private val url = "https://www.uio.no/studier/emner/matnat/ifi/IN2000/v24/obligatoriske-oppgaver/district3.json"
@@ -27,13 +26,13 @@ class AggregatedVotesDataSource {
     }
 
     suspend fun getAggregatedVotesThree(): List<DistrictVotes> {
-        val response: PartiesD =
+        val response: PartiesVote =
             try {
                 Log.d("YIKES", "check check found $url")
                 client.get(url).body()
             } catch (e: Exception) {
                 Log.e("YIKES", "Couldn't fetch data from url $url")
-                PartiesD(emptyList())
+                PartiesVote(emptyList())
             }
 
         return response.parties.map {
